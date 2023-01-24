f233ce152412394a9b486a0b423d17c1ae5b0a5b
Singony <122940695+Singony@users.noreply.github.com>
2023-01-24 11:59:20 -0300

    Commit Final CV

---
+++ Botones.js
@@ -0,0 +1,6 @@
+document.getElementById("boton_mostrar").addEventListener('click',function() {
+    document.getElementById("Info").style.display = 'block';
+    })
+    document.getElementById("boton_ocultar").addEventListener('click',function() {
+        document.getElementById("Info").style.display = 'none';
+    })
